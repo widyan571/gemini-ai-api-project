23388175b14913293c7da0f0a59460dadb5069f2
Banteng Widayntoro <bantengwidyantoroparkour@gmail.com>
2025-07-17 11:22:42 +0700

    set temperature 0.5

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({
     model: 'gemini-2.5-flash',
     generationConfig: {
-        temperature: 0
+        temperature: 0.5
     }
 });
 
